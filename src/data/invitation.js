c0ffd9184a5b50681eaa4c00c93e81cbe4d16265
Erik Espinosa <erikespinosa@Mac-mini-de-Erik.local>
2026-09-18 00:14:46 -0600

    feat: adjust invitation details

--- src/data/invitation.js
+++ src/data/invitation.js
@@ -4,17 +4,18 @@ export const invitation = {
   },
 
   event: {
-    date: "24 de octubre de 2026",
-    dateTime: "2026-10-24T13:00:00-06:00",
+    date: "21 de noviembre de 2026",
+    dateTime: "2026-11-21T13:00:00-06:00",
     time: "13:00",
-    ceremony: "Ceremonia religiosa",
-    location: "Parroquia de ejemplo",
-    address: "Ciudad de México",
-    mapsUrl: "https://maps.google.com/"
+    ceremony: "Parroquia San Ildefonso Amealco",
+    location: "Casa Ejidal de la comunidad de la muralla",
+    address: "Amealco, Queretaro",
+    mapsUrlCeremony: "https://share.google/nfSaugDJOA7kses5R",
+    mapsUrlEvent: "https://maps.app.goo.gl/69bZee6gaeQKS3XL6?g_st=aw"
   },
 
   whatsapp: {
-    phone: "521XXXXXXXXXX"
+    phone: "5215541049029"
   },
 
   gallery: [
